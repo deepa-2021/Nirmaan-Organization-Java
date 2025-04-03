55fb3d67cd2e5cd34a99739da3a91a0d236f2601
deepa-2021 <deepanadrajan152004@gmail.com>
2025-04-03 15:59:25 +0530

    day02
    variable tasks

---
+++ variableTask.java
@@ -0,0 +1,25 @@
+package day2;
+
+public class variableTask{
+	public static void main(String[]arg) {
+	byte num=120;
+	short a=32767;
+	int n=3333;
+	float f=12.45f;
+	double d=1234.4567;
+	char c='a';
+	boolean value=true;
+	
+	
+	
+	
+	System.out.println(num);
+	System.out.println(a);
+	System.out.println(n);
+	System.out.println(f);
+	System.out.println(d);
+	System.out.println(c);
+	System.out.println(value);
+	}
+}
+	
