8bc58753c1c77f7a3d5dea7e5242c0af5ef44db1
deepa-2021 <deepanadrajan152004@gmail.com>
2025-04-08 11:24:20 +0530

    day4
    condition

---
+++ condition.java
@@ -0,0 +1,28 @@
+package day5;
+import java.util.Scanner;
+public class condition {
+	
+	
+	
+	public static void main(String[]args){
+		
+	Scanner sc= new Scanner(System.in);
+	System.out.println("Enter your mark ");
+	int mark=sc.nextInt();
+	System.out.println("your mark is"+mark);
+	
+	if (mark>=35) {
+		System.out.println("you are pass");
+		}
+		else {
+			System.out.println("your are fail");
+			 }
+	}
+	
+	
+
+	
+	
+	}
+
+
