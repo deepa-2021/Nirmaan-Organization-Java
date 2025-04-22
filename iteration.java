ccb14bbc8178ef6bd73e71710e6770a639dbef23
deepa-2021 <deepanadrajan152004@gmail.com>
2025-04-22 11:54:17 +0530

    day8
    iteration

---
+++ iteration.java
@@ -0,0 +1,27 @@
+package day8;
+
+import java.util.Scanner;
+
+public class iteration {
+	
+public static void main(String[]args) {
+	
+Scanner sc=new Scanner(System.in);
+
+System.out.println("enter the number");
+
+  
+int iteration=sc.nextInt();
+
+for( int i=1;i<=iteration;i++) {
+	System.out.println(" iteration is:"+i);
+for(int j=1;j<=6;j++) {
+	System.out.println(" iteration is:"+j);
+}
+	
+}
+
+
+}
+
+}
